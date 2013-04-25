264a5b0bbfde2cef2fee8a77cda840068d280e79
Gavin Carr <gavin@openfusion.com.au>
2013-04-25 14:27:53 +1000

    Bump $VERSION to 0.44, update Changes.

--- Tabulate.pm
+++ Tabulate.pm
@@ -12,7 +12,7 @@ require Exporter;
 @EXPORT = qw();
 @EXPORT_OK = qw(&render);
 
-$VERSION = '0.43';
+$VERSION = '0.44';
 my $DEFAULT_TEXT_FORMAT = "<p>%s</p>\n";
 my %DEFAULT_DEFN = (
     style       => 'down', 
